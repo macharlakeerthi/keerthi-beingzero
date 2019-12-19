5ddc2e67da55792cf247a69b7d0649f77427630c
macharlakeerthi <59007919+macharlakeerthi@users.noreply.github.com>
2019-12-19 15:49:51 +0530

    Making site work

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/public/htmls/home.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
